ed023ef569798c5fd5a9f07a8cef080d5f09b067
Lars Carius <lars.carius@yahoo.de>
2021-09-14 16:04:34 +0200

    adds nodetype filesystemglb, expands local and web objects example, implements loading from apps documents directory on Android

--- example/lib/examples/localandwebobjectsexample.dart
+++ example/lib/examples/localandwebobjectsexample.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
 import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
 import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
 import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
 import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
@@ -10,6 +13,7 @@ import 'package:ar_flutter_plugin/models/ar_node.dart';
 import 'package:flutter/services.dart';
 import 'package:vector_math/vector_math_64.dart';
 import 'dart:math';
+import 'package:path_provider/path_provider.dart';
 
 class LocalAndWebObjectsWidget extends StatefulWidget {
   LocalAndWebObjectsWidget({Key key}) : super(key: key);
@@ -25,6 +29,8 @@ class _LocalAndWebObjectsWidgetState extends State<LocalAndWebObjectsWidget> {
   ARNode localObjectNode;
   //String webObjectReference;
   ARNode webObjectNode;
+  ARNode fileSystemNode;
+  HttpClient httpClient;
 
   @override
   Widget build(BuildContext context) {
@@ -46,8 +52,16 @@ class _LocalAndWebObjectsWidgetState extends State<LocalAndWebObjectsWidget> {
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     ElevatedButton(
+                        onPressed: onFileSystemObjectAtOriginButtonPressed,
+                        child: Text("Add/Remove Filesystem\nObject at Origin")),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
                         onPressed: onLocalObjectAtOriginButtonPressed,
-                        child: Text("Add/Remove Local\nobject at Origin")),
+                        child: Text("Add/Remove Local\nObject at Origin")),
                     ElevatedButton(
                         onPressed: onWebObjectAtOriginButtonPressed,
                         child: Text("Add/Remove Web\nObject at Origin")),
@@ -84,6 +98,23 @@ class _LocalAndWebObjectsWidgetState extends State<LocalAndWebObjectsWidget> {
           handleTaps: false,
         );
     this.arObjectManager.onInitialize();
+
+    //Download model to file system
+    httpClient = new HttpClient();
+    _downloadFile(
+        "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF-Binary/Duck.glb",
+        "LocalDuck.glb");
+  }
+
+  Future<File> _downloadFile(String url, String filename) async {
+    var request = await httpClient.getUrl(Uri.parse(url));
+    var response = await request.close();
+    var bytes = await consolidateHttpClientResponseBytes(response);
+    String dir = (await getApplicationDocumentsDirectory()).path;
+    File file = new File('$dir/$filename');
+    await file.writeAsBytes(bytes);
+    print("Downloading finished, path: " + '$dir/$filename');
+    return file;
   }
 
   Future<void> onLocalObjectAtOriginButtonPressed() async {
@@ -114,6 +145,20 @@ class _LocalAndWebObjectsWidgetState extends State<LocalAndWebObjectsWidget> {
           scale: Vector3(0.2, 0.2, 0.2));
       bool didAddWebNode = await this.arObjectManager.addNode(newNode);
       this.webObjectNode = (didAddWebNode) ? newNode : null;
+    }
+  }
+
+  Future<void> onFileSystemObjectAtOriginButtonPressed() async {
+    if (this.fileSystemNode != null) {
+      this.arObjectManager.removeNode(this.fileSystemNode);
+      this.fileSystemNode = null;
+    } else {
+      var newNode = ARNode(
+          type: NodeType.fileSystemGLB,
+          uri: "LocalDuck.glb",
+          scale: Vector3(0.2, 0.2, 0.2));
+      bool didAddFileSystemNode = await this.arObjectManager.addNode(newNode);
+      this.fileSystemNode = (didAddFileSystemNode) ? newNode : null;
     }
   }
 
--- lib/datatypes/node_types.dart
+++ lib/datatypes/node_types.dart
@@ -1,5 +1,6 @@
 /// Determines which types of nodes the plugin supports
 enum NodeType {
-  localGLTF2,
-  webGLB,
+  localGLTF2, // Node with renderable with fileending .gltf in the Flutter asset folder
+  webGLB, // Node with renderable with fileending .glb loaded from the internet during runtime
+  fileSystemGLB, // Node with renderable with fileending .glb in the documents folder of the current app
 }
