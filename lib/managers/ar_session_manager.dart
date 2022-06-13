02067cb67b582276c8a5b8de60cdceec452ab31f
denkoul <dennis.koulouris@outlook.com>
2022-06-13 11:15:47 +0300

    get distance methods

--- lib/managers/ar_session_manager.dart
+++ lib/managers/ar_session_manager.dart
@@ -1,10 +1,13 @@
+import 'dart:math' show sqrt;
 import 'dart:typed_data';
+
+import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
+import 'package:ar_flutter_plugin/models/ar_anchor.dart';
 import 'package:ar_flutter_plugin/models/ar_hittest_result.dart';
+import 'package:ar_flutter_plugin/utils/json_converters.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
-import 'package:ar_flutter_plugin/utils/json_converters.dart';
-
+import 'package:vector_math/vector_math_64.dart';
 
 // Type definitions to enforce a consistent use of the API
 typedef ARHitResultHandler = void Function(List<ARHitTestResult> hits);
@@ -26,8 +29,7 @@ class ARSessionManager {
   /// Receives hit results from user taps with tracked planes or feature points
   late ARHitResultHandler onPlaneOrPointTap;
 
-  ARSessionManager(int id, this.buildContext, this.planeDetectionConfig,
-      {this.debug = false}) {
+  ARSessionManager(int id, this.buildContext, this.planeDetectionConfig, {this.debug = false}) {
     _channel = MethodChannel('arsession_$id');
     _channel.setMethodCallHandler(_platformCallHandler);
     if (debug) {
@@ -35,7 +37,7 @@ class ARSessionManager {
     }
   }
 
-  /// Returns the camera pose with respect to the world coordinate system of the [ARView]
+  /// Returns the camera pose in Matrix4 format with respect to the world coordinate system of the [ARView]
   Future<Matrix4?> getCameraPose() async {
     try {
       final serializedCameraPose = await _channel.invokeMethod<List<dynamic>>('getCameraPose', {});
@@ -46,14 +48,55 @@ class ARSessionManager {
     }
   }
 
-  Future getPose(String anchorName) async {
+  /// Returns the given anchor pose in Matrix4 format with respect to the world coordinate system of the [ARView]
+  Future<Matrix4?> getPose(ARAnchor anchor) async {
     try {
-      final serializedCameraPose = await _channel.invokeMethod<List<dynamic>>('getAnchorPose', {"anchorId": anchorName});
+      if (anchor.name.isEmpty) {
+        throw Exception("Anchor can not be resolved. Anchor name is empty.");
+      }
+      final serializedCameraPose = await _channel.invokeMethod<List<dynamic>>('getAnchorPose', {
+        "anchorId": anchor.name,
+      });
       return MatrixConverter().fromJson(serializedCameraPose!);
     } catch (e) {
       print('Error caught: ' + e.toString());
       return null;
     }
+  }
+
+  /// Returns the distance in meters between @anchor1 and @anchor2.
+  Future<double?> getDistanceBetweenAnchors(ARAnchor anchor1, ARAnchor anchor2) async {
+    var anchor1Pose = await getPose(anchor1);
+    var anchor2Pose = await getPose(anchor2);
+    var anchor1Translation = anchor1Pose?.getTranslation();
+    var anchor2Translation = anchor2Pose?.getTranslation();
+    if (anchor1Translation != null && anchor2Translation != null) {
+      return getDistanceBetweenVectors(anchor1Translation, anchor2Translation);
+    } else {
+      return null;
+    }
+  }
+
+  /// Returns the distance in meters between @anchor and device's camera.
+  Future<double?> getDistanceFromAnchor(ARAnchor anchor) async {
+    Matrix4? cameraPose = await getCameraPose();
+    Matrix4? anchorPose = await getPose(anchor);
+    Vector3? cameraTranslation = cameraPose?.getTranslation();
+    Vector3? anchorTranslation = anchorPose?.getTranslation();
+    if (anchorTranslation != null && cameraTranslation != null) {
+      return getDistanceBetweenVectors(anchorTranslation, cameraTranslation);
+    } else {
+      return null;
+    }
+  }
+
+  /// Returns the distance in meters between @vector1 and @vector2.
+  double getDistanceBetweenVectors(Vector3 vector1, Vector3 vector2) {
+    num dx = vector1.x - vector2.x;
+    num dy = vector1.y - vector2.y;
+    num dz = vector1.z - vector2.z;
+    double distance = sqrt(dx * dx + dy * dy + dz * dz);
+    return distance;
   }
 
   Future<void> _platformCallHandler(MethodCall call) {
@@ -71,10 +114,7 @@ class ARSessionManager {
         case 'onPlaneOrPointTap':
           if (onPlaneOrPointTap != null) {
             final rawHitTestResults = call.arguments as List<dynamic>;
-            final serializedHitTestResults = rawHitTestResults
-                .map(
-                    (hitTestResult) => Map<String, dynamic>.from(hitTestResult))
-                .toList();
+            final serializedHitTestResults = rawHitTestResults.map((hitTestResult) => Map<String, dynamic>.from(hitTestResult)).toList();
             final hitTestResults = serializedHitTestResults.map((e) {
               return ARHitTestResult.fromJson(e);
             }).toList();
@@ -124,11 +164,7 @@ class ARSessionManager {
   /// Displays the [errorMessage] in a snackbar of the parent widget
   onError(String errorMessage) {
     ScaffoldMessenger.of(buildContext).showSnackBar(SnackBar(
-        content: Text(errorMessage),
-        action: SnackBarAction(
-            label: 'HIDE',
-            onPressed:
-                ScaffoldMessenger.of(buildContext).hideCurrentSnackBar)));
+        content: Text(errorMessage), action: SnackBarAction(label: 'HIDE', onPressed: ScaffoldMessenger.of(buildContext).hideCurrentSnackBar)));
   }
 
   /// Dispose the AR view on the platforms to pause the scenes and disconnect the platform handlers.
