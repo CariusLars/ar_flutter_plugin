9672053b7c6659cd770c56b25719749ebbf9c3f4
denkoul <dennis.koulouris@outlook.com>
2022-07-07 18:35:57 +0300

    fix ios issue

--- ios/Classes/IosARView.swift
+++ ios/Classes/IosARView.swift
@@ -190,6 +190,9 @@ class IosARView: NSObject, FlutterPlatformView, ARSCNViewDelegate, UIGestureReco
                 arcoreSession = try! GARSession.session()
 
                 if (arcoreSession != nil){
+                    let configuration = GARSessionConfiguration();
+                    configuration.cloudAnchorMode = .enabled;
+                    arcoreSession?.setConfiguration(configuration, error: nil)
                     if let token = JWTGenerator().generateWebToken(){
                         arcoreSession!.setAuthToken(token)
 
